5505cf24be923c961b217983f4edf75343b86945
mhendricks96 <mhendricks96@yahoo.com>
2021-05-01 16:41:54 -0700

    only modal left

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // const { schema } = require('./models/schema');
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/bookAPI', { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -58,13 +59,34 @@ app.get('/book', (req, res) => {
   console.log(user);
   // get all books from db
   // send them in response
-  User.find( {userEmail: user}, (err, databaseResults) => {
+  User.find({ userEmail: user }, (err, databaseResults) => {
     // send them in my response to front end
     res.send(databaseResults[0]);
   });
 });
 
+app.post('/book', (req, res) => {
+  let user = req.query.user;
+  // uses app.json above
+  console.log(req.query.user);
+  //find user in database
+  User.find({ userEmail: user }, (err, userData) => {
+    if (userData.length < 1) {
+      res.send(400).send('user does not exist');
+    } else {//add book info to user
+      userData[0].favoriteBooks.push({
+        bookName: req.body.name,
+        bookDescription: req.body.description,
+      });
+      //save user
+      userData[0].save().then(() => {
+        res.send('user saved');
+      });
+    }
+  });
 
+
+});
 // route to get 1 user from db
 // app.get('/user', (req,res)=> {
 
@@ -73,6 +95,17 @@ app.get('/book', (req, res) => {
 //     res.send(databaseResults);
 //   });
 // });
+app.delete('/books/:id', (req, res) => {
+  let email = req.query.user;
+  User.find({ email: email }, (err, userData) => {
+    let user = userData[0];
+    user.books = user.books.filter(book => `${book._id}` !== req.paramm.id);
+    //save user
+    user.save().then(userData => {
+      res.send(userData.gifts);
+    });
+  });
+});
 
 app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
 
